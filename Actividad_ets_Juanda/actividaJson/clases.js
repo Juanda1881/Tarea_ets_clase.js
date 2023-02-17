cd9a909c107ac2067a05312f6f9433d42eedf267
Juanda1881 <116909835+Juanda1881@users.noreply.github.com>
2023-02-17 18:44:36 +0000

    Add files via upload

---
+++ Actividad_ets_Juanda/actividaJson/clases.js
@@ -0,0 +1,23 @@
+const Producto1={
+Categoria: "Decoracion",
+Precio: "5.99$",
+Descripcion: "Figura maya de 7cm de alto hecha de madera",
+Foto: "Foto del articulo"
+
+}
+
+const Producto2={
+    Categoria: "Decoracion",
+    Precio: "15.99$",
+    Descripcion: "Jarron de ceramica estilo griego",
+    Foto: "Foto del articulo"
+    
+    }
+
+    const Producto3={
+        Categoria: "Utilidad",
+        Precio: "20.99$",
+        Descripcion: "Pack de 5 cuchillos de cocina",
+        Foto: "Foto del articulo"
+        
+        }
